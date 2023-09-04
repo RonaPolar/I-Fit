3c663bc7fab8abf6bb1dd3824baa3c053a18478a
RonaPolar <ronatecson3@gmail.com>
2023-09-04 14:51:31 +0800

    sheesh

--- lib/screens/user/home.dart
+++ lib/screens/user/home.dart
@@ -13,7 +13,7 @@ class _HomeState extends State<Home> {
     return const Scaffold(
       body: Center(
         child: Text(
-          'Home, On Standby,whoop'
+          'Home, On Standby'
         )
       ),
       
